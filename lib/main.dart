d5d0a134b15ba4429178e460431a59c524b4bf25
1gnoramus <elenwarface@gmail.com>
2022-11-26 18:57:12 +0600

    they re moveing rn!

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ class MyGame extends FlameGame {
   SpriteComponent background = SpriteComponent();
 
   final double characterSize = (200);
+  bool turnAway = false;
   @override
   Future<void> onLoad() async {
     super.onLoad();
@@ -39,14 +40,22 @@ class MyGame extends FlameGame {
       ..sprite = await loadSprite('knight.png')
       ..size = Vector2(characterSize, characterSize)
       ..y = screenHeight - characterSize - textBoxHeight
-      ..x = screenWidth - characterSize;
+      ..x = screenWidth - characterSize
+      ..flipHorizontally();
     add(knight);
   }
 
   @override
   void update(double dt) {
     super.update(dt);
-    queen.x += 30 * dt;
-    knight.x -= 30 * dt;
+    if (queen.x < size[0] - 100) {
+      queen.x += 30 * dt;
+    } else if (turnAway == false) {
+      knight.flipHorizontally;
+      turnAway = true;
+    }
+    if (knight.x > size[0] - 50) {
+      knight.x -= 30 * dt;
+    }
   }
 }
