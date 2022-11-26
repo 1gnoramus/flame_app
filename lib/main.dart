d2a4b2ff7903715627ec1a7db03c69258beaaab7
1gnoramus <elenwarface@gmail.com>
2022-11-26 18:49:20 +0600

    sprites added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,52 @@
+import 'package:flame/components.dart';
+import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
+import 'package:flame/flame.dart';
 
 void main() {
   print('sad');
+  runApp(
+    GameWidget(
+      game: MyGame(),
+    ),
+  );
+}
+
+class MyGame extends FlameGame {
+  SpriteComponent queen = SpriteComponent();
+  SpriteComponent knight = SpriteComponent();
+  SpriteComponent background = SpriteComponent();
+
+  final double characterSize = (200);
+  @override
+  Future<void> onLoad() async {
+    super.onLoad();
+    print('object');
+
+    final screenWidth = size[0];
+    final screenHeight = size[1];
+    final textBoxHeight = 100;
+    add(background
+      ..sprite = await loadSprite('background_1.jpg')
+      ..size = size);
+    queen
+      ..sprite = await loadSprite('queen.png')
+      ..size = Vector2(characterSize, characterSize)
+      ..y = screenHeight - characterSize - textBoxHeight;
+
+    add(queen);
+    knight
+      ..sprite = await loadSprite('knight.png')
+      ..size = Vector2(characterSize, characterSize)
+      ..y = screenHeight - characterSize - textBoxHeight
+      ..x = screenWidth - characterSize;
+    add(knight);
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+    queen.x += 30 * dt;
+    knight.x -= 30 * dt;
+  }
 }
